3fea9bc0599441019166abdaa87854342b52d9fd
Abner Chang <abner.chang@hpe.com>
2019-09-23 11:40:35 +0800

    U500Pkg/SerialIoLib: Add copyrights
    Add HPE copyrights.
    
    Signed-off-by: Abner Chang <abner.chang@hpe.com>
    
    Cc: Leif Lindholm <leif.lindholm@linaro.org>
    Cc: Gilbert Chen <gilbert.chen@hpe.com>

--- Platform/RiscV/SiFive/U500Pkg/Library/SerialIoLib/SerialPortLib.c
+++ Platform/RiscV/SiFive/U500Pkg/Library/SerialIoLib/SerialPortLib.c
@@ -1,6 +1,8 @@
 /** @file
   UART Serial Port library functions
 
+  Copyright (c) 2019, Hewlett Packard Enterprise Development LP. All rights reserved.<BR>
+
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
